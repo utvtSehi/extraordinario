c92b354eae254775301b6a2ec5f66eef10eed680
Gahandi Sebastian Cervantes Hinojosa <gsch.sehi@gmail.com>
2024-04-24 21:28:34 -0600

    Corrección de errores 3

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const morgan = require('morgan');
 const routes = require('./routes');
 const port = process.env.PORT || 3005;
 const app = express();
+const cors = require('cors');
+
 require('./config/mongo');
 require('dotenv').config();
 
 const expressListEndpoints = require('express-list-endpoints');
 
+    app.use(cors());
     app.use('/api', require('./routes/index'))
     app.use(morgan('dev'));
     console.log(expressListEndpoints(app));
