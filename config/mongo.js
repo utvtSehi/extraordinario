c551bfce82f358ce09874893c920f58670d030f4
Gahandi Sebastian Cervantes Hinojosa <gsch.sehi@gmail.com>
2024-04-24 21:15:34 -0600

    Corrección de errores

--- config/mongo.js
+++ config/mongo.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb + srv://al222210096:qfihIJUZalo6kIzn@cluster0.feluvoh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-.then(() => {
+mongoose.connect('mongodb+srv://al222210096:qfihIJUZalo6kIzn@cluster0.feluvoh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
     console.log('Conexión exitosa a MongoDB.');
-})
-.catch((error) => {
+}).catch((error) => {
     console.error('Error de conexión a MongoDB:', error);
 });
